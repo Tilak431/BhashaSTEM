614a8ba48f4c56726ef490e99c92334b6bddac59
Tilak Bant <tilakbant617@gmail.com>
2026-01-14 17:31:14 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/(app)/quizzes/[quizId]/page.tsx
+++ src/app/(app)/quizzes/[quizId]/page.tsx
@@ -527,6 +527,6 @@ function QuestionDisplay({
 }
 
 
-export default function QuizPage({ params }: { params: { quizId: string } }) {
-  return <QuizClientView quizId={params.quizId} />;
+export default function QuizPage({ params: { quizId } }: { params: { quizId: string } }) {
+  return <QuizClientView quizId={quizId} />;
 }
