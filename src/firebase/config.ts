729605ac3fbe030d489a7aa31c8f4699658396fa
Tilak Bant <tilakbant617@gmail.com>
2026-01-14 15:34:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1583566680-db9a1",
+  "appId": "1:939367806556:web:b9c445c3c3d2e9d1241899",
+  "apiKey": "AIzaSyCP342dftu2n_TsmotWAPMWgbXJTIe9cWc",
+  "authDomain": "studio-1583566680-db9a1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "939367806556"
+};
